66b1355fa2b2e25110fcfad26b160e0fc898ef05
despird-zh <despird.zhang@yahoo.com>
2017-03-02 12:09:11 +0800

    update

--- app/src/components/mdPageNavsbar/index.js
+++ app/src/components/mdPageNavsbar/index.js
@@ -5,6 +5,5 @@ import MdPageNavsbarTheme from './mdPageNavsbar.theme';
 export default function install(Vue) {
   Vue.component('md-page-nav', Vue.extend(MdPageNav));
   Vue.component('md-page-navs', Vue.extend(MdPageNavsbar));
-
   Vue.material.styles.push(MdPageNavsbarTheme);
 }
