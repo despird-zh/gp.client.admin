1e3988c8c6bf18618dc7274a07344d7ccbd062b4
gary-dgc <gary.diao@yahoo.com>
2017-03-05 09:55:53 +0800

    Revert "remove plain input"
    This reverts commit 605d2ec7436f503b42a7f64dde2cb8d3bac51bbf.

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -3,11 +3,11 @@ import Vue from 'vue';
 
 import PageNavsbar from './mdPageNavsbar';
 import LogonDialog from './mdLogonDialog';
-//import PlainInput from './mdPlainInput';
+import PlainInput from './mdPlainInput';
 // custom theme
 import MdExtTheme from '../assets/styles/mdl-ext.theme';
 Vue.material.styles.push(MdExtTheme);
 
 Vue.use(PageNavsbar);
 Vue.use(LogonDialog);
-//Vue.use(PlainInput);
+Vue.use(PlainInput);
---
+++ app/src/components/mdPlainInput/mdPlainInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <input
+    class="gp-input"
+    :type="type"
+    :value="value"
+    :disabled="disabled"
+    :required="required"
+    :placeholder="placeholder"
+    :maxlength="maxlength"
+    @focus="onFocus"
+    @blur="onBlur"
+    @input="onInput"
+    @keydown.up="onInput"
+    @keydown.down="onInput">
+</template>
+
+<style lang="scss" src="./mdPlainInput.scss"></style>
+
+<script>
+  import themeMixin from '../../utils/themeMixin';
+
+  export default {
+    mixins: [themeMixin],
+    props: {
+      type: {
+        type: String,
+        default: 'text'
+      },
+      value: [String, Number],
+      disabled: Boolean,
+      required: Boolean,
+      maxlength: [Number, String],
+      placeholder: String
+    },
+    data: () => {
+      return {
+        isFocused: false
+      };
+    },
+    computed: {
+      classes() {
+        return {
+          'md-input-placeholder': this.hasPlaceholder,
+          'md-input-disabled': this.disabled,
+          'md-input-required': this.required,
+          'md-input-focused': this.isFocused
+        };
+      }
+    },
+    methods: {
+      hasPlaceholder() {
+        return this.placeholder !== '';
+      },
+      onInput() {
+        this.$emit('change', this.$el.value);
+        this.$emit('input', this.$el.value);
+      },
+      getValue() {
+        return this.$el.value;
+      },
+      onFocus() {
+        this.isFocused = true;
+      },
+      onBlur() {
+        this.isFocused = false;
+      }
+    }
+  };
+</script>
