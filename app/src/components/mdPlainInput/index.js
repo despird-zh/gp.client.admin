605d2ec7436f503b42a7f64dde2cb8d3bac51bbf
gary-dgc <gary.diao@yahoo.com>
2017-03-05 09:44:06 +0800

    remove plain input

--- app/src/components/mdPlainInput/index.js
+++
@@ -1,8 +0,0 @@
-import MdPlainInput from './mdPlainInput.vue';
-import MdPlainInputTheme from './mdPlainInput.theme';
-
-export default function install(Vue) {
-  Vue.component('md-plain-input', Vue.extend(MdPlainInput));
-
-  Vue.material.styles.push(MdPlainInputTheme);
-}
