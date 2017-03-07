f4307be818f9c93faa570b307e2770d131cbd477
despird-zh <despird.zhang@yahoo.com>
2017-03-07 19:20:55 +0800

    not ready token refresh

--- app/src/utils/rpcMixin.js
+++ app/src/utils/rpcMixin.js
@@ -16,7 +16,7 @@ export default {
 
       if (this.jwttoken) {
         httpOpts['headers'] = {
-          'Auth-Token': this.jwttoken
+          'Authorization': 'Bearer: ' + this.jwttoken
         };
       }
       return httpOpts;
@@ -24,6 +24,50 @@ export default {
     $httpUrl(url) {
 
       return this.baseUrl + url;
+    },
+    $refreshToken() {
+      return new Promise((resolve, reject) => {
+            //Refresh token
+            let _options = {
+              headers: {
+                'Authorization': 'Bearer: ' + this.jwttoken
+              }
+            };
+
+            Vue.http({url: this.$httpUrl('reissue.do'), options: _options}).then( (response) => {
+                //Store refreshed token
+                localStorage.setItem('id_token', response.data.token);
+                //Resubmit original request and resolve the response (probably shouldn't be the responsibility of the Auth service...)
+                Vue.http(request).then(function (newResponse) {
+                    resolve(newResponse);
+                });
+            }, function (newResponse) {
+                reject(newResponse);
+            });
+        });
     }
   }
 };
+//https://laracasts.com/discuss/channels/vue/jwt-auth-with-vue-resource-interceptor
+//define the VueResource intercepters
+Vue.http.interceptors.push(function (request, next) {
+    // 
+    if(this.authenticated){
+      //Add JWT to all requests
+      request.headers.set('Authorization', 'Bearer: ' + this.jwttoken);
+    }
+    request.headers.set('Accept', 'application/json');
+
+    //Skip storing token refresh requests
+    next( (response) => {
+        //Check for expired token response, if expired, refresh token and resubmit original request
+        let respdata = response.body;
+
+        if(respdata.meta.code === 'TOKEN_EXPIRED'){
+
+        }
+        auth.checkExpiredToken(response, request).then(function(response) {
+            return response;
+        })
+    });
+});
