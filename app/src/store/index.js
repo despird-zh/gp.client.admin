8a1ed211fdd3af0c3d3643c37728ad73fe243481
despird-zh <despird.zhang@yahoo.com>
2017-03-09 10:38:19 +0800

    import the vuex logger in store

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
 import createPersistedState from 'vuex-persistedstate';
 
 import * as actions from './actions';
 import * as getters from './getters';
 import { state, mutations } from './mutations';
-//import createLogger from 'vuex/logger';
 
 Vue.use(Vuex);
 
@@ -17,7 +17,8 @@ export default new Vuex.Store({
   getters,
   mutations,
   plugins: [
-    createPersistedState({ storage: window.sessionStorage })
+    createPersistedState({ storage: window.sessionStorage }),
+    createLogger()
   ],
   strict: debug
 });
